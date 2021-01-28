a318354b50a6d63617360a89b8046622b2fb6fad
Mante <35216654+mantevian@users.noreply.github.com>
2021-01-28 19:16:20 +0300

    copypaste error

--- utils/save_guild_settings.js
+++ utils/save_guild_settings.js
@@ -5,8 +5,8 @@ module.exports = function (client) {
     client.guilds.cache.forEach(async (g) => {
         var guild_settings = (await database.get_guild_config_value(g.id, `settings`)).val();
         if (guild_settings)
-            client.message_settings.set(g.id, guild_settings);
+            client.guild_settings.set(g.id, guild_settings);
         else
-            client.message_settings.set(g.id, undefined);
+            client.guild_settings.set(g.id, undefined);
     });
 }
